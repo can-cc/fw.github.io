7db7bad1ea9b77a962536f16a438d7e1ce23883c
fwchen <crystal.tyan@gmail.com>
2017-03-13 23:32:38 +0800

    initial commit

--- app/index.js
+++ app/index.js
@@ -1,12 +1,48 @@
-import _ from 'lodash';
+import Three from 'three';
 
-function component () {
-  var element = document.createElement('div');
+console.log('hi');
 
-  /* lodash is required for the next line to work */
-  element.innerHTML = _.join(['Hello','webpack'], ' ');
+// window.onload = function(){
 
-  return element;
-}
+//     var renderer = new THREE.WebGLRenderer();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     document.body.appendChild(renderer.domElement);
 
-document.body.appendChild(component());
+//     // camera
+//     var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 1, 1000);
+//     camera.position.y = -250;
+//     camera.position.z = 400;
+//     camera.rotation.x = 45 * (Math.PI / 180);
+
+//     // scene
+//     var scene = new THREE.Scene();
+//     scene.add(camera); //ADDED
+
+//     var img = new THREE.MeshBasicMaterial({ //CHANGED to MeshBasicMaterial
+//         map:THREE.ImageUtils.loadTexture('img/front.jpg')
+//     });
+//     img.map.needsUpdate = true; //ADDED
+
+//     // plane
+//     var plane = new THREE.Mesh(new THREE.PlaneGeometry(200, 200),img);
+//     plane.overdraw = true;
+//     scene.add(plane);
+
+//      // add subtle ambient lighting
+//     var ambientLight = new THREE.AmbientLight(0x555555);
+//     scene.add(ambientLight);
+
+//     // add directional light source
+//     var directionalLight = new THREE.DirectionalLight(0xffffff);
+//     directionalLight.position.set(1, 1, 1).normalize();
+//     scene.add(directionalLight);
+
+//     // create wrapper object that contains three.js objects
+//     var three = {
+//         renderer: renderer,
+//         camera: camera,
+//         scene: scene,
+//         plane: plane
+//     };
+//     renderer.render(scene,camera);
+// };
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,26 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: 'babel-loader',
+        exclude: [
+          path.resolve(__dirname, 'node_modules')
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ]
 };
